e662f82b7613ab208944e86c5f2a7218d3d03c68
Isabella <isams1211@gmail.com>
2022-10-20 22:37:22 -0300

    Add files via upload

---
+++ resolução_LP4_1.js
@@ -0,0 +1,13 @@
+/*
+Crie um programa que verifique se um determinado número é impar ou par!
+Caso seja ímpar retorne "O número X é ímpar"
+Caso seja par retorne "O número X é par"
+*/
+
+let numero = 3;
+
+if (numero % 2) {
+    console.log(`O numero ${numero} é ímpar`)
+} else {
+    console.log(`O numero ${numero} é par`)
+}
